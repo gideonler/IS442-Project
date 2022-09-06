8f29b8c32cb5f471b0451034fb0795d10bd602b3
gideonler <gideonler@gmail.com>
2022-09-06 17:45:02 +0800

    Your description

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <hello-world/>
+</template>
+<!-- importing HelloWorld.vue component -->
+
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
---
+++ src/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{msg}}</h1>
+</template>
+<!-- msg displayed at the top and defaulted to an empty string -->
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return{
+      msg: ''
+    }
+  },
+  mounted(){
+    fetch("/api/messages/hello")
+    .then((response)=>response.text())
+    .then((data)=>{
+      this.msg=data;
+
+    })
+  }
+}
+</script>
+
