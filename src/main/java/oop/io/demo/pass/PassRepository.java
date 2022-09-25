c0a4a0803e5b48186850f3a46a1f9b10a95c0e55
Khant Min Naing <naing.khantmin.learning@gmail.com>
2022-09-25 16:41:12 +0800

    Deleted (old) Passes

--- src/main/java/oop/io/demo/pass/PassRepository.java
+++
@@ -1,9 +0,0 @@
-package oop.io.demo.pass;
-import java.util.*;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PassRepository extends MongoRepository<Pass, String> {
-    Optional<Pass> findByPassName(String passName);
-
-    boolean existsByPassName(String email);
-}
