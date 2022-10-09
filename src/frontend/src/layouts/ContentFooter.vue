c5ad55ab801b7a89bf40fa4960816ffa208cabe9
Lee Xin Yi <89073263+lee-xin-yi@users.noreply.github.com>
2022-10-09 23:28:25 +0800

    Sticky Footer

--- src/frontend/src/layouts/ContentFooter.vue
+++ src/frontend/src/layouts/ContentFooter.vue
@@ -1,26 +1,34 @@
 <template>
+  <div class="sticky-footer">
     <footer class="footer mt-auto">
-    <div class="container-fluid">
-      <nav>
-        <ul class="footer-menu">
-          <li>
-            <router-link :to="{path:'/booking'}">Booking</router-link>
-          </li>
-        </ul>
-      </nav>
-      <div class="copyright text-center">
-        &copy; Singapore Sports School.
-        Designed by G2T4.
+      <div class="container-fluid">
+        <!-- <nav>
+          <ul class="footer-menu">
+            <li>
+              <router-link :to="{path:'/booking'}">Booking</router-link>
+            </li>
+          </ul>
+        </nav> -->
+        <div class="copyright text-center">
+          &copy; Singapore Sports School.
+          Designed by G2T4.
+        </div>
       </div>
-    </div>
-  </footer>
+    </footer>
+  </div>
 
 
 </template>
 <script>
-  export default {}
+export default {}
 
 </script>
-<style>
 
+<style>
+.sticky-footer {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+}
 </style>
