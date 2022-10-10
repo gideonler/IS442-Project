1de0420904bc6826bccc3ce3c85033d8c6b1b094
Tan Xin Yi <08tanx@gmail.com>
2022-10-10 11:59:59 +0800

    edit setting of username to be automatic- to test

--- src/main/java/oop/io/demo/user/User.java
+++ src/main/java/oop/io/demo/user/User.java
@@ -33,9 +33,9 @@ public class User {
 
     public User() {}
 
-    public User(String name, String username, String email) {
+    public User(String name,  String email) {
         this.email= email;
-        this.username = username;
+        this.username = email.split("@")[0];
         this.name = name;
         this.isVerified=false;
         this.userType = USERTYPE.STAFF;
@@ -91,12 +91,12 @@ public class User {
         return isVerified;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public double getOutStandingFees() {
