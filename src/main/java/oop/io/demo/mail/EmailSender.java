949b6e0c3386a63f56e37f596b4324bf6b8136d9
Yeo Shi <yeoshitan@gmail.com>
2022-11-01 22:44:12 +0800

    Update request mapping for email

--- src/main/java/oop/io/demo/mail/EmailSender.java
+++ src/main/java/oop/io/demo/mail/EmailSender.java
@@ -26,7 +26,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/send")
+@RequestMapping("/email")
 public class EmailSender {
     @Autowired
     EmailService emailService;
