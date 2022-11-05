e77c70b6e14aa9c89718b37897b8587cedaa481d
Lee Xin Yi <89073263+lee-xin-yi@users.noreply.github.com>
2022-11-06 04:18:22 +0800

    login

---
+++ src/frontend/src/views/Register.vue
@@ -0,0 +1,110 @@
+<template>
+
+  <div>
+    <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+      <li class="nav-item" role="presentation">
+        <a class="nav-link" id="tab-login" data-mdb-toggle="pill" href="#pills-login" role="tab"
+          aria-controls="pills-login" aria-selected="false">Login</a>
+      </li>
+      <li class="nav-item" role="presentation">
+        <a class="nav-link active" id="tab-register" data-mdb-toggle="pill" href="#pills-register" role="tab"
+          aria-controls="pills-register" aria-selected="true">Register</a>
+      </li>
+    </ul>
+
+    <b-form @submit.stop.prevent="onSubmit">
+      <b-form-group id="example-input-group-1" label="Name" label-for="example-input-1">
+        <b-form-input id="example-input-1" name="example-input-1" v-model="$v.form.name.$model"
+          :state="validateState('name')" aria-describedby="input-1-live-feedback"></b-form-input>
+
+        <b-form-invalid-feedback id="input-1-live-feedback">This is a required field.</b-form-invalid-feedback>
+      </b-form-group>
+
+      <b-form-group id="example-input-group-3" label="Email" label-for="example-input-3">
+        <b-form-input id="example-input-3" name="example-input-3" v-model="$v.form.email.$model"
+          :state="validateState('email')" aria-describedby="input-3-live-feedback"></b-form-input>
+
+        <b-form-invalid-feedback id="input-3-live-feedback">This is a required field and the minimum length is 6.
+        </b-form-invalid-feedback>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Get Activation Link</b-button>
+    
+      <!-- <b-button class="ml-2" @click="resetForm()">Reset</b-button> -->
+
+      <!-- <b-button variant="primary" v-b-modal.modal-prevent-closing>Get Activation Link</b-button> -->
+      <!-- <b-modal id="modal-prevent-closing" ref="modal" title="Check your email for activation link" @show="resetModal"
+        @hidden="resetModal" @ok="handleOk">
+        <form ref="form" @submit.stop.prevent="handleSubmit">
+          <b-form-group label="Paste activation link" label-for="name-input" invalid-feedback="Link is required"
+            :state="nameState">
+            <b-form-input id="name-input" v-model="name" :state="nameState" required></b-form-input>
+          </b-form-group>
+        </form>
+      </b-modal> -->
+    </b-form>
+  </div>
+</template>
+
+<style>
+body {
+  padding: 1rem;
+}
+</style>
+
+<script>
+import { validationMixin } from "vuelidate";
+import { required, minLength } from "vuelidate/lib/validators";
+
+export default {
+  mixins: [validationMixin],
+  data() {
+    return {
+      form: {
+        name: null,
+        email: null,
+      },
+      status: 'not_accepted',
+      link: ''
+    };
+  },
+  validations: {
+    form: {
+      name: {
+        required
+      },
+      email: {
+        required
+      }
+    }
+  },
+  methods: {
+    validateState(name) {
+      const { $dirty, $error } = this.$v.form[name];
+      return $dirty ? !$error : null;
+    },
+    validateState(email) {
+      const { $dirty, $error } = this.$v.form[email];
+      return $dirty ? !$error : null;
+    },
+    // resetForm() {
+    //   this.form = {
+    //     email: null,
+    //     food: null
+    //   };
+
+    //   this.$nextTick(() => {
+    //     this.$v.$reset();
+    //   });
+    // },
+    onSubmit() {
+      this.$v.form.$touch();
+      if (this.$v.form.$anyError) {
+        return;
+      }
+      this.$router.push('/authentication')
+      alert("Form submitted!");
+    }
+  }
+};
+</script>
