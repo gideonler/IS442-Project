1e480a3d90f2621d3f0f0bde01620191611bbb6b
pearlynlohwq <pblyn11@gmail.com>
2022-11-13 00:23:40 +0800

    integration for view passes

--- src/frontend/src/components/AdminCorpPassManagement/PassDataTable.vue
+++ src/frontend/src/components/AdminCorpPassManagement/PassDataTable.vue
@@ -172,6 +172,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import BEditableTable from "bootstrap-vue-editable-table";
 import {
   BIconTrash,
@@ -192,120 +193,156 @@ export default {
   data() {
     return {
       placeOfInterest: '',
+      // api: { view_passes: "http://localhost:8080/pass/passes" + this.placeOfInterest
+      // },
+      api: { view_passes: "http://localhost:8080/pass/passes/" + "Duck%20Tours"
+      },
+      items: [],
       fields: [
         { key: "delete", label: "" },
         {
-          key: "name",
-          label: "Name",
+          key: "passId",
+          label: "Pass Id",
           type: "text",
           editable: true,
           placeholder: "Enter Name...",
-          class: "name-col",
+          class: "id-col",
           validate: this.validateName
         },
         {
-          key: "department",
-          label: "Department",
+          key: "passNo",
+          label: "Pass No.",
+          type: "text",
+          editable: true,
+          class: "pass-no-col",
+        },
+        {
+          key: "passStatus",
+          label: "Pass Status",
           type: "select",
           editable: true,
-          class: "department-col",
+          class: "status-col",
           options: [
-            { value: 1, text: "HR" },
-            { value: 2, text: "Engineer" },
-            { value: 3, text: "VP" },
-            { value: 4, text: "CEO" },
+            { value: 1, text: "INOFFICE" },
+            { value: 2, text: "OUT" },
           ],
-        },
-        {
-          key: "age",
-          label: "Age",
-          type: "range",
-          min: "0",
-          max: "100",
-          editable: true,
-          placeholder: "Enter Age...",
-          class: "age-col",
-        },
-        {
-          key: "dateOfBirth",
-          label: "Date Of Birth",
-          type: "date",
-          editable: true,
-          class: "date-col",
-          locale: "en",
-          "date-format-options": {
-            year: "numeric",
-            month: "numeric",
-            day: "numeric",
-          },
-        },
-        {
-          key: "isActive",
-          label: "Is Active",
-          type: "checkbox",
-          editable: true,
-          class: "is-active-col",
         },
         { key: "edit", label: "" },
       ],
-      items: [
-        {
-          id: 1,
-          age: 40,
-          name: "Dickerson",
-          department: 1,
-          dateOfBirth: "1984-05-20",
-          isActive: true,
-        },
-        {
-          id: 2,
-          age: 21,
-          name: "Larsen",
-          department: 2,
-          dateOfBirth: "1972-07-25",
-          isActive: false,
-        },
-        {
-          id: 3,
-          age: 89,
-          name: "Geneva",
-          department: 3,
-          dateOfBirth: "1981-02-02",
-          isActive: false,
-        },
-        {
-          id: 1,
-          age: 40,
-          name: "Dickerson",
-          department: 1,
-          dateOfBirth: "1984-05-20",
-          isActive: true,
-        },
-        {
-          id: 2,
-          age: 21,
-          name: "Larsen",
-          department: 2,
-          dateOfBirth: "1972-07-25",
-          isActive: false,
-        },
-        {
-          id: 3,
-          age: 89,
-          name: "Geneva",
-          department: 3,
-          dateOfBirth: "1981-02-02",
-          isActive: false,
-        },
-        {
-          id: 4,
-          age: 38,
-          name: "Jami",
-          department: 4,
-          dateOfBirth: "1964-10-19",
-          isActive: true,
-        },
-      ],
+      // fields: [
+      //   { key: "delete", label: "" },
+      //   {
+      //     key: "name",
+      //     label: "Name",
+      //     type: "text",
+      //     editable: true,
+      //     placeholder: "Enter Name...",
+      //     class: "name-col",
+      //     validate: this.validateName
+      //   },
+      //   {
+      //     key: "department",
+      //     label: "Department",
+      //     type: "select",
+      //     editable: true,
+      //     class: "department-col",
+      //     options: [
+      //       { value: 1, text: "HR" },
+      //       { value: 2, text: "Engineer" },
+      //       { value: 3, text: "VP" },
+      //       { value: 4, text: "CEO" },
+      //     ],
+      //   },
+      //   {
+      //     key: "age",
+      //     label: "Age",
+      //     type: "range",
+      //     min: "0",
+      //     max: "100",
+      //     editable: true,
+      //     placeholder: "Enter Age...",
+      //     class: "age-col",
+      //   },
+      //   {
+      //     key: "dateOfBirth",
+      //     label: "Date Of Birth",
+      //     type: "date",
+      //     editable: true,
+      //     class: "date-col",
+      //     locale: "en",
+      //     "date-format-options": {
+      //       year: "numeric",
+      //       month: "numeric",
+      //       day: "numeric",
+      //     },
+      //   },
+      //   {
+      //     key: "isActive",
+      //     label: "Is Active",
+      //     type: "checkbox",
+      //     editable: true,
+      //     class: "is-active-col",
+      //   },
+      //   { key: "edit", label: "" },
+      // ],
+      // items: [
+      //   {
+      //     id: 1,
+      //     age: 40,
+      //     name: "Dickerson",
+      //     department: 1,
+      //     dateOfBirth: "1984-05-20",
+      //     isActive: true,
+      //   },
+      //   {
+      //     id: 2,
+      //     age: 21,
+      //     name: "Larsen",
+      //     department: 2,
+      //     dateOfBirth: "1972-07-25",
+      //     isActive: false,
+      //   },
+      //   {
+      //     id: 3,
+      //     age: 89,
+      //     name: "Geneva",
+      //     department: 3,
+      //     dateOfBirth: "1981-02-02",
+      //     isActive: false,
+      //   },
+      //   {
+      //     id: 1,
+      //     age: 40,
+      //     name: "Dickerson",
+      //     department: 1,
+      //     dateOfBirth: "1984-05-20",
+      //     isActive: true,
+      //   },
+      //   {
+      //     id: 2,
+      //     age: 21,
+      //     name: "Larsen",
+      //     department: 2,
+      //     dateOfBirth: "1972-07-25",
+      //     isActive: false,
+      //   },
+      //   {
+      //     id: 3,
+      //     age: 89,
+      //     name: "Geneva",
+      //     department: 3,
+      //     dateOfBirth: "1981-02-02",
+      //     isActive: false,
+      //   },
+      //   {
+      //     id: 4,
+      //     age: 38,
+      //     name: "Jami",
+      //     department: 4,
+      //     dateOfBirth: "1964-10-19",
+      //     isActive: true,
+      //   },
+      // ],
       rowUpdate: {},
       totalRows: 1,
           currentPage: 1,
@@ -390,6 +427,16 @@ export default {
     this.$root.$refs.PassDataTable= this;
   },
   mounted(){
+    axios
+            .get(api.view_passes)
+            .then((response) => {
+                var passes_list = response.data;
+                console.log(passes_list);
+                this.items= passes_list;
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     this.totalRows = this.items.length
   }
 };
@@ -429,23 +476,17 @@ table.editable-table td {
   font-size: 20px;
 }
 
-.name-col {
+.id-col {
+  width:  15%;
+}
+
+.pass-no-col {
+  width: 40%;
+}
+
+
+.status-col {
   width:  20%;
 }
 
-.department-col {
-  width: 20%;
-}
-
-.age-col {
-  width:  10%;
-}
-
-.date-col {
-  width:  20%;
-}
-
-.is-active-col {
-  width:  10%;
-}
 </style>
