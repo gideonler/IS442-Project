0ea1a1a91a972073f02ee43c9308729c52770fb2
Yeo Shi <yeoshitan@gmail.com>
2022-11-13 12:07:16 +0800

    Emails Scheduler Done!

--- src/main/java/oop/io/demo/loan/LoanRepository.java
+++ src/main/java/oop/io/demo/loan/LoanRepository.java
@@ -16,6 +16,7 @@ public interface LoanRepository extends MongoRepository<Loan, String>{
    
     ArrayList<Loan> findAllByUserEmail(String userEmail);
     ArrayList<Loan> findAllByAttractionName(String attractionName);
+    ArrayList<Loan> findAllByStatus(String status);
     Loan findByLoanId(String loanId);
     Loan findByUserEmail(String userEmail);
     ArrayList<Loan> findAllByPassNo(String passNo);
