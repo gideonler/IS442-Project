5ba4ed1fca7c08b0e1b47d57d5f475fc310e3e7e
pearlynlohwq <pblyn11@gmail.com>
2022-11-15 20:43:31 +0800

    integrate no. of available passes and selected date

--- src/frontend/src/views/Booking.vue
+++ src/frontend/src/views/Booking.vue
@@ -1,36 +1,42 @@
 <template>
-<div class="content">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-7" >
-          <CalendarFilter></CalendarFilter>
-         <Calendar fluid></Calendar>
-        
-        </div>
-        <div class="col-md-5">
-          <Sidebar></Sidebar>
+  <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-7" >
+           <CalendarFilter @filter-attraction="filterAttraction" ></CalendarFilter>
+           <Calendar :key="this.selected_attraction" :selected_attraction="this.selected_attraction" fluid></Calendar>
+           <Sidebar></Sidebar>
+          </div>
+          <div class="col-md-5">
+            <Sidebar></Sidebar>
+          </div>
         </div>
       </div>
     </div>
-  </div>
- </template>
- 
- 
- <script>
-    import DashboardLayout from '../layouts/DashboardLayout'
-    import Calendar from '../components/Booking/Calendar'
-    import CalendarFilter from '../components/Booking/CalendarFilter.vue';
-    import Sidebar from '../components/Booking/Sidebar.vue';
-    
-    export default {
-            name: "booking-page",
-            components: { Calendar, CalendarFilter, Sidebar },
-            created() {
-                this.$emit('update:layout', DashboardLayout);
-            },
-            
-        };
-
-</script>
-
-
+   </template>
+   
+   
+   <script>
+      import DashboardLayout from '../layouts/DashboardLayout'
+      import Calendar from '../components/Booking/Calendar'
+      import CalendarFilter from '../components/Booking/CalendarFilter.vue';
+      import Sidebar from '../components/Booking/Sidebar.vue';
+      
+      export default {
+              name: "booking-page",
+              components: { Calendar, CalendarFilter, Sidebar },
+              data(){
+                return {
+                  selected_attraction: null,
+                }
+              },
+              created() {
+                  this.$emit('update:layout', DashboardLayout);
+              },
+              methods: {
+                filterAttraction(selected_attraction) {
+                  this.selected_attraction = selected_attraction;
+                }
+              }
+          };
+  </script>
