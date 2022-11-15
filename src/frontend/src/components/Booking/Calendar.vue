6d88b2facc46ddb4a03e40975d6d11fde6fa46a8
pearlynlohwq <pblyn11@gmail.com>
2022-11-15 18:12:56 +0800

    integrate calendar availabilities

--- src/frontend/src/components/Booking/Calendar.vue
+++ src/frontend/src/components/Booking/Calendar.vue
@@ -7,7 +7,8 @@
     import BookingPopUp from './BookingPopUp.vue';
     import WaitingListPopUp from './WaitingListPopUp.vue';
     import BookingConfirmation from './BookingConfirmation.vue';
-import WaitingListConfirmation from './WaitingListConfirmation.vue';
+    import WaitingListConfirmation from './WaitingListConfirmation.vue';
+    import axios from 'axios';
 
     export default {
         name:'calendar-component',
@@ -20,6 +21,10 @@ import WaitingListConfirmation from './WaitingListConfirmation.vue';
 },
       data() {
         return {
+          api: {
+            get_avail_passes: "http://localhost:8080/loan/availpasses" ,
+          },
+          avail_passes: [],
           calendarOptions: {
             headerToolbar: {
               left: 'prev,next today',
@@ -47,17 +52,85 @@ import WaitingListConfirmation from './WaitingListConfirmation.vue';
             dateClick: this.handleDateClick,
             
             //TODO: replace this dummy data with data from the backend: 
-            events: [
-              { title: 'unavailable', date: '2022-09-01' ,   display: 'background',    color: '#ff7f7f'},
-              { title: 'unavailable', date: '2022-09-06' ,   display: 'background',    color: '#ff7f7f'},
-              { title: 'unavailable', date: '2022-09-09' ,   display: 'background',    color: '#ff7f7f'},
-              { title: 'available', date: '2022-09-02',   display: 'background'  }
-            ],
+            // events: [
+            //   { title: 'unavailable', date: '2022-09-01' ,   display: 'background',    color: '#ff7f7f'},
+            //   { title: 'unavailable', date: '2022-09-06' ,   display: 'background',    color: '#ff7f7f'},
+            //   { title: 'unavailable', date: '2022-09-09' ,   display: 'background',    color: '#ff7f7f'},
+            //   { title: 'available', date: '2022-09-02',   display: 'background'  }
+            // ],
+            events: [],
             showModal: false
           }
         }
       },
+      created() {
+        this.loadData();
+    },
     methods: {
+      async loadData(){
+        await axios
+          .get(this.api.get_avail_passes)
+          .then((response) => {
+            this.total_loans = response.data
+
+
+            var selected_attraction= "Mandai Zoo"
+            var availabilities= response.data[selected_attraction]
+            var result= []
+
+
+            const today = new Date()
+            var tomorrow =  new Date()
+            tomorrow.setDate(today.getDate() + 1)
+            var last =  new Date()
+            last.setDate(today.getDate() + 56)
+            
+            // var dd = String(tomorrow.getDate()).padStart(2, '0');
+            // var mm = String(tomorrow.getMonth() + 1).padStart(2, '0'); //January is 0!
+            // var yyyy = tomorrow.getFullYear();
+            var avail_dates = []
+            var daysOfYear = [];
+            for (var d = tomorrow; d <= last; d.setDate(d.getDate() + 1)) {
+                daysOfYear.push(new Date(d));
+                var date= new Date(d)
+                var dd = String(date.getDate()).padStart(2, '0');
+                var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+                var yyyy = date.getFullYear();
+                avail_dates.push(yyyy+"-" +mm+ "-"+ dd)
+            }
+
+            console.log(avail_dates)
+            var temp;
+            for(date of avail_dates){
+              if(this.containsKey(availabilities,date)){
+                //all available
+                if(availabilities[date]!=0){
+                  temp= { title: 'available', 'date': date,   display: 'background', passes_left: availabilities[date]  }
+                }
+              }
+              //all still available
+              else{
+                temp= { title: 'available', 'date': date,   display: 'background'}
+              }
+              result.push(temp)
+            }
+            var total_availabilities= this.total_loans[selected_attraction]
+
+            console.log(result)
+            this.calendarOptions.events= result;
+
+          })
+          .catch((error) => {
+              if (error) {
+                  console.log(error);
+              }
+          });
+        
+          
+        },
+        containsKey(obj, key ) {
+        return Object.keys(obj).includes(String(key));
+        },
         toggleWeekends: function() {
         this.calendarOptions.weekends = !this.calendarOptions.weekends // toggle the boolean!
         },
