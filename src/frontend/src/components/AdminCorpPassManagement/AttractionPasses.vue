afe870ebb2f2ca96de64558e53597d6451ae8ca5
pearlynlohwq <pblyn11@gmail.com>
2022-11-16 02:07:31 +0800

    styling for attraction pass controls

--- src/frontend/src/components/AdminCorpPassManagement/AttractionPasses.vue
+++ src/frontend/src/components/AdminCorpPassManagement/AttractionPasses.vue
@@ -1,16 +1,25 @@
     <template >
     <div >
+
         
+
+
         <b-card no-body>
+            
     <b-tabs card>
+   
       <b-tab title="Active" style="background-color: #f7f7f8;" active>
-            <b-carousel
+
+        <b-carousel
         class="mx-5"
         id="all-passes-carousel"
         controls
         no-animation
+        :interval="0"
     >
-    <b-carousel-slide  :key="index" v-for="index in no_active_card_groups">
+    
+    
+    <b-carousel-slide :key="index" v-for="index in no_active_card_groups">
         <template #img>
             <b-card-group deck >    
                 <b-card style="max-width:33.33%;" :key="attraction.attractionName"  v-for="attraction in active_attractions.slice((index-1)*3,((index-1)*3)+3)" 
@@ -25,13 +34,17 @@
                     <b-button  @click="editDetails(attraction.attractionName, attraction.replacementFee, attraction.templateFilename,attraction.attachmentPDFFilename, attraction.imageFilename, attraction.passType)" variant="success" class="mx-1">Edit Details</b-button>
                     <b-button @click=viewPasses(attraction.attractionName) class="mx-1">View Passes</b-button>
                 </template>
+
+                
             </b-card>
         </b-card-group>
         </template>
     </b-carousel-slide>
     </b-carousel>
-      </b-tab>
 
+
+      </b-tab>
+    
       <b-tab title="Inactive" style="background-color: #f7f7f8;" >
             <b-carousel
         class="mx-5"
@@ -63,7 +76,6 @@
 
     </b-tabs>
   </b-card>
-
 
    
   <div hidden>
@@ -146,5 +158,6 @@
     },
     
     }
+    
   </script>
 
