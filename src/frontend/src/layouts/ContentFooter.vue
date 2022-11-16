91e67c2a46c56001422c4f6a71acb0429934dc0b
pearlynlohwq <pblyn11@gmail.com>
2022-11-16 16:23:50 +0800

    frontend clean up

--- src/frontend/src/layouts/ContentFooter.vue
+++ src/frontend/src/layouts/ContentFooter.vue
@@ -2,13 +2,6 @@
   <div class="sticky-footer">
     <footer class="footer mt-auto">
       <div class="container-fluid">
-        <!-- <nav>
-          <ul class="footer-menu">
-            <li>
-              <router-link :to="{path:'/booking'}">Booking</router-link>
-            </li>
-          </ul>
-        </nav> -->
         <div class="copyright text-center">
           &copy; Singapore Sports School.
           Designed by G2T4.
